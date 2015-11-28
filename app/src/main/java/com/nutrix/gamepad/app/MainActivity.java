1f99d977cdce626fbc3c97039d7909f2e7a512f5
onewhl <prystjam@gmail.com>
2015-11-28 21:06:36 +0300

    removed bad code

--- app/src/main/java/com/nutrix/gamepad/app/MainActivity.java
+++ app/src/main/java/com/nutrix/gamepad/app/MainActivity.java
@@ -3,15 +3,12 @@ package com.nutrix.gamepad.app;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.os.AsyncTask;
 import android.view.Window;
 import android.view.WindowManager;
 import drawField.Field;
 import networking.Client;
 
 public class MainActivity extends Activity {
-
-    private Client mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,27 +18,9 @@ public class MainActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(new Field(this));
-        new connectTask().execute("");
-    }
-
-    public class connectTask extends AsyncTask<String, String, Client> {
-
-        @Override
-        protected Client doInBackground(String... message) {
-
-            //create a Client object
-            mClient = new Client(new Client.OnMessageReceived() {
-                @Override
-                //here the messageReceived method is implemented
-                public void messageReceived(String message) {
-                    //this method calls the onProgressUpdate
-                    publishProgress(message);
-                }
-            });
-            mClient.run();
-
-            return null;
-        }
+        new Client().execute();
     }
 }
+
